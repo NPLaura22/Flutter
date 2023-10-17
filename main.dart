d217ee371e29246b4bb0f59370526598a091b18d
NPLaura22 <99835188+NPLaura22@users.noreply.github.com>
2023-10-17 10:58:44 -0300

    PROVA DDM - Lista de Tarefas - 17/10

--- main.dart
+++ main.dart
@@ -1,101 +1,26 @@
-// ignore_for_file: prefer_const_constructors
-
-//import 'dart:js_util';
-
-import 'package:flutter/material.dart'; //impo´rta bibliotecas
+import 'package:flutter/material.dart';
+import 'package:prova_ddm_nplaura/tarefa.dart';
 
 void main() {
-  runApp(const MyApp()); //roda o app
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget { //MyApp: classe, com mtodos e atributos; extends: heranca
-  const MyApp({super.key}); //construtor (mesmo nome classe); super: chama construtor da superclasse
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
 
-  // This widget is the root of your application.
-  @override 
-  Widget build(BuildContext context) {  //metodo build recebe como parametro context e retorna widget
-    return MaterialApp( //MaterialApp: é um topo´de widget
-      title: 'Primeiro Proj LL',
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NPLaura - Prova DDM',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.purple,
       ),
-      home: Scaffold(
-        appBar: AppBar (
-          title: Text("Água Santa"),
-          backgroundColor: Color.fromARGB(255, 1, 124, 161),
-          centerTitle: true,
-        ),  //AppBar é um widget
-        body: Center(          
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [ 
-              SizedBox(height: 10,),
-              DecoratedBox(
-                decoration: BoxDecoration (color: Colors.blueAccent),
-                child: Padding (
-                  padding: EdgeInsets.all(18),
-                  child: Text("Eu", style: TextStyle(fontSize: 30),),
-                )
-              ),
-              SizedBox(height: 30), //ou poderia ser Divider
-              DecoratedBox(
-                decoration: BoxDecoration (color: Color.fromARGB(255, 223, 35, 35)),
-                child: Padding (
-                  padding: EdgeInsets.all(20),
-                  child: Text("amo", style: TextStyle(fontSize: 30),),
-                ) 
-              ),
-              SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  DecoratedBox(
-                  decoration: BoxDecoration (color: Colors.deepPurple),
-                  child: Padding (
-                  padding: EdgeInsets.all(10),
-                  
-                  child: Text("a", style: TextStyle(fontSize: 60),),
-                )
-              ),
-                  SizedBox(width: 10,),
-                  DecoratedBox(
-                  decoration: BoxDecoration (color: Color.fromARGB(255, 91, 211, 141)),
-                  child: Padding (
-                  padding: EdgeInsets.all(10),                 
-                  child: Text("aula", style: TextStyle(fontSize: 60),),
-                )
-              ),
-                  SizedBox(width: 10,),
-                  DecoratedBox(
-                  decoration: BoxDecoration (color: Color.fromARGB(255, 22, 151, 29)),
-                  child: Padding (
-                  padding: EdgeInsets.all(10),
-                  child: Text("da", style: TextStyle(fontSize: 60),),
-                )
-              ),
-                ],
-              ),
-              SizedBox(height: 30), //ou poderia ser Divider
-              DecoratedBox(
-                decoration: BoxDecoration (color: Color.fromARGB(255, 158, 30, 141)),
-                child: Padding (
-                  padding: EdgeInsets.all(20),
-                  child: Text("Tânia", style: TextStyle(fontSize: 30),),
-                ) 
-              ),
-             SizedBox(height: 20,),
-              SizedBox(
-                height: 150,
-                width: 200,
-                child: Image.network('https://cdn.pixabay.com/photo/2018/01/10/23/53/rabbit-3075088_960_720.png'),  
-              ),
-              
-            ], //vetor
-          ),
-        ),
-      )
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Tarefa(), 
+      
+      },
     );
   }
 }
---
+++ tarefa.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class Tarefa extends StatefulWidget {
+  @override
+  _Tarefa createState() => _Tarefa();
+}
+
+class _Tarefa extends State<Tarefa> {
+  List<String> totalCampos = [];
+  String novoCampo = "";
+
+  void addTodo() {
+    if (novoCampo.isNotEmpty) {
+      setState(() {
+        totalCampos.add(novoCampo);
+        novoCampo = ""; 
+      });
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Erro'),
+            content: Text('Campo "tarefas" é obrigatório'),
+            actions: <Widget>[
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  void removeTodo(int index) {
+    setState(() {
+      totalCampos.removeAt(index);
+    });
+  }
+
+  void clearList() {
+    setState(() {
+      totalCampos.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Tarefas'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(                    
+                    onChanged: (text) {
+                      setState(() {
+                        novoCampo = text;
+                      });
+                    },
+                    onSubmitted: (text) {
+                      addTodo();
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Tarefas:',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.adb),
+                  onPressed: addTodo,
+                ),
+                
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: totalCampos.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(totalCampos[index]),
+                  trailing: IconButton(
+                    icon: Icon(Icons.bug_report),
+                    onPressed: () {
+                      removeTodo(index);
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+          ElevatedButton(
+            onPressed: clearList,
+            child: Text('Limpar lista'),            
+          ),
+          SizedBox(height: 30,)
+        ],
+      ),
+    );
+  }
+}
